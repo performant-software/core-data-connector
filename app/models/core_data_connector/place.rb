92063fc46759dd4aed89473b3cb4ef6f8a76c7a5
Derek Leadbetter <dleadbetter55@gmail.com>
2023-08-31 11:33:23 -0400

    CDCN #2 - Adding name validation to place model

--- app/models/core_data_connector/place.rb
+++ app/models/core_data_connector/place.rb
@@ -12,8 +12,21 @@ module CoreDataConnector
     # Resourceable parameters
     allow_params place_names_attributes: [:id, :name, :primary, :_destroy]
 
+    # Validations
+    validate :validate_place_names
+
     def name
       primary_name&.name
+    end
+
+    private
+
+    def has_primary_name?
+      place_names.select{ |pn| pn.primary? }.present?
+    end
+
+    def validate_place_names
+      errors.add(:place_names, I18n.t('errors.place.primary_name')) unless has_primary_name?
     end
   end
 end
