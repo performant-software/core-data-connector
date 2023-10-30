80e71fcba10e5c5cb4b7425362a255213cbeeed7
dleadbetter <>
2023-10-30 06:32:27 -0400

    CDC #23 - Updating ownable_controller to only accept "project_model_id" parameter for index route

--- app/controllers/concerns/core_data_connector/ownable_controller.rb
+++ app/controllers/concerns/core_data_connector/ownable_controller.rb
@@ -11,13 +11,10 @@ module CoreDataConnector
         # For a single record, we don't need to owner_id or owner_type
         return query if params[:id].present?
 
-        if params[:project_model_id].present?
-          query.where(project_model_id: params[:project_model_id])
-        elsif params[:project_id].present?
-          query.joins(:project_model).where(core_data_connector_project_models: { project_id: params[:project_id] })
-        else
-          item_class.none
-        end
+        # Return an empty set if the project_model_id is not present
+        return item_class.none unless params[:project_model_id].present?
+
+        query.where(project_model_id: params[:project_model_id])
       end
     end
   end
