b8cc3069791ceac6cf18d32fb9ab270f10b2fc91
dleadbetter <>
2023-11-08 16:22:08 -0500

    CDC #35 - Renaming place_geometries_serializer arguments

--- app/serializers/core_data_connector/place_geometries_serializer.rb
+++ app/serializers/core_data_connector/place_geometries_serializer.rb
@@ -1,6 +1,6 @@
 module CoreDataConnector
   class PlaceGeometriesSerializer < BaseSerializer
-    index_attributes :id, geometry_json: -> (pm, *rest) { pm.to_geojson }
-    show_attributes :id, geometry_json: -> (pm, *rest) { pm.to_geojson }
+    index_attributes :id, geometry_json: -> (pg, *rest) { pg.to_geojson }
+    show_attributes :id, geometry_json: -> (pg, *rest) { pg.to_geojson }
   end
 end
