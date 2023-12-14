af0524e386f825b10c8f4fa9b0897fb186ad7470
Camden Mecklem <c@camdenmecklem.com>
2023-12-14 12:13:06 -0500

    fix query

--- app/controllers/core_data_connector/names_controller.rb
+++ app/controllers/core_data_connector/names_controller.rb
@@ -8,23 +8,21 @@ module CoreDataConnector
 
       return query.none unless params[:project_id] && params[:nameable_model]
 
+      nameable_table = params[:nameable_model].to_sym
       nameable_model = "CoreDataConnector::#{params[:nameable_model].classify}".constantize
-      polymorphic_table_class = "CoreDataConnector::#{nameable_model.get_names_table.to_s.classify}".constantize
+
+      polymorphic_table = nameable_model.get_names_table
+      polymorphic_table_class = "CoreDataConnector::#{polymorphic_table.to_s.classify}".constantize
 
       query
         .where(
           polymorphic_table_class
-            .where(polymorphic_table_class.arel_table[:name_id].eq(CoreDataConnector::Name.arel_table[:id]))
-            .arel
-            .exists
-        )
-        .where(
-          nameable_model
-            .joins(nameable_model.get_names_table.to_sym => :name)
-            .joins(:project_model)
+            .joins(nameable_table.to_s.singularize.to_sym => :project_model)
             .where(
               CoreDataConnector::ProjectModel.arel_table[:project_id].eq(params[:project_id])
-            )
+              )
+            .where(polymorphic_table_class.arel_table[:nameable_type].eq(nameable_model.to_s))
+            .where(polymorphic_table_class.arel_table[:name_id].eq(CoreDataConnector::Name.arel_table[:id]))
             .arel
             .exists
         )
--- app/models/core_data_connector/source_title.rb
+++ app/models/core_data_connector/source_title.rb
@@ -3,6 +3,10 @@ module CoreDataConnector
     belongs_to :name
     belongs_to :nameable, polymorphic: true
 
+    belongs_to :instance, foreign_key: :nameable_id, optional: true
+    belongs_to :item, foreign_key: :nameable_id, optional: true
+    belongs_to :work, foreign_key: :nameable_id, optional: true
+
     accepts_nested_attributes_for :name
   end
 end
