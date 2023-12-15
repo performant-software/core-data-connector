7c7278f4f18aa76138fec4d71410ad74a0a9d349
dleadbetter <>
2023-12-15 06:23:49 -0500

    CDC #79 - Updates the resource_api gem to latest; Adds overrides to relatable_controller for JSON response; Refactors the linked_open_data_serializer

--- app/serializers/core_data_connector/public/organizations_serializer.rb
+++ app/serializers/core_data_connector/public/organizations_serializer.rb
@@ -3,6 +3,7 @@ module CoreDataConnector
     class OrganizationsSerializer < LinkedOpenDataSerializer
       index_attributes(:id) { |organization|  "#{ENV['HOSTNAME']}/public/organizations/#{organization.uuid}" }
       index_attributes(:title) { |organization| organization.name }
+      index_attributes(:type) { 'Organization' }
       index_attributes :description, user_defined: UserDefinedSerializer
     end
   end
