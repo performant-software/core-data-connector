c0418dd5576b9457c0eab9698afda291d9275098
dleadbetter <>
2023-12-19 13:29:18 -0500

    CDC #79 - Adding record_id attribute to public serializers

--- app/serializers/core_data_connector/public/organizations_serializer.rb
+++ app/serializers/core_data_connector/public/organizations_serializer.rb
@@ -2,6 +2,7 @@ module CoreDataConnector
   module Public
     class OrganizationsSerializer < LinkedOpenDataSerializer
       index_attributes(:id) { |organization|  "#{ENV['HOSTNAME']}/public/organizations/#{organization.uuid}" }
+      index_attributes(:record_id) { |organization| organization.id }
       index_attributes(:title) { |organization| organization.name }
       index_attributes(:type) { 'Organization' }
       index_attributes :description, user_defined: UserDefinedSerializer
--- app/serializers/core_data_connector/public/people_serializer.rb
+++ app/serializers/core_data_connector/public/people_serializer.rb
@@ -2,6 +2,7 @@ module CoreDataConnector
   module Public
     class PeopleSerializer < LinkedOpenDataSerializer
       index_attributes(:id) { |person| "#{ENV['HOSTNAME']}/public/people/#{person.uuid}" }
+      index_attributes(:record_id) { |person| person.id }
       index_attributes(:title) { |person| person.full_name }
       index_attributes(:type){ 'Person' }
       index_attributes :biography, user_defined: UserDefinedSerializer
