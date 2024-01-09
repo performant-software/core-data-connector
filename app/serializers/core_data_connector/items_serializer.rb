01f085bcf1aad782993ca6d51b3f5470f249430d
Camden <c@camdenmecklem.com>
2024-01-09 15:29:45 -0500

    fix missing UDFs from source serializers (#38)

--- app/serializers/core_data_connector/items_serializer.rb
+++ app/serializers/core_data_connector/items_serializer.rb
@@ -1,6 +1,7 @@
 module CoreDataConnector
   class ItemsSerializer < BaseSerializer
     include OwnableSerializer
+    include UserDefinedFields::FieldableSerializer
 
     index_attributes :id, primary_name: SourceTitlesSerializer, source_titles: SourceTitlesSerializer
     show_attributes :id, primary_name: SourceTitlesSerializer, source_titles: SourceTitlesSerializer
