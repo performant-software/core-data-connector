5b02ee0f3f0868f2eceedbca479e53b627d7079b
Camden <c@camdenmecklem.com>
2024-01-25 15:03:17 -0500

    add jisc (#58)

--- app/models/core_data_connector/web_authority.rb
+++ app/models/core_data_connector/web_authority.rb
@@ -1,6 +1,6 @@
 module CoreDataConnector
   class WebAuthority < ApplicationRecord
-    SOURCE_TYPES = %w(atom dpla wikidata)
+    SOURCE_TYPES = %w(atom dpla jisc wikidata)
 
     # Relationships
     belongs_to :project
