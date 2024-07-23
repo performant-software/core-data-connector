cd16070be03e8a6116ccbddea88d5ddf6ae73370
dleadbetter <derek@performantsoftware.com>
2024-07-23 13:03:36 -0400

    CDC #248 - Adding "geometry" attribute back to public API endpoints for backwards compatibility

--- app/serializers/core_data_connector/public/v0/linked_places/place_layers_serializer.rb
+++ app/serializers/core_data_connector/public/v0/linked_places/place_layers_serializer.rb
@@ -4,7 +4,10 @@ module CoreDataConnector
       module LinkedPlaces
         class PlaceLayersSerializer < BaseSerializer
           index_attributes :id, :name, :layer_type, :content, :url
+          index_attributes(:geometry) { |layer| layer.content }
+
           show_attributes :id, :name, :layer_type, :content, :url
+          show_attributes(:geometry) { |layer| layer.content }
         end
       end
     end
