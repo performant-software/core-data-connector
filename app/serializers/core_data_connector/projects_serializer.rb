32176e101d99570bfb49b3b1ea2513526c61243e
Ben Silverman <ben@performantsoftware.com>
2025-02-05 09:42:00 -0500

    Add map_library_url to permitted attrs, serializer (performant-software/core-data-cloud#380)

--- app/serializers/core_data_connector/projects_serializer.rb
+++ app/serializers/core_data_connector/projects_serializer.rb
@@ -1,6 +1,6 @@
 module CoreDataConnector
   class ProjectsSerializer < BaseSerializer
-    index_attributes :id, :name, :description, :discoverable, :faircopy_cloud_url, :faircopy_cloud_project_model_id
-    show_attributes :id, :name, :description, :discoverable, :faircopy_cloud_url, :faircopy_cloud_project_model_id
+    index_attributes :id, :name, :description, :discoverable, :faircopy_cloud_url, :map_library_url, :faircopy_cloud_project_model_id
+    show_attributes :id, :name, :description, :discoverable, :faircopy_cloud_url, :map_library_url, :faircopy_cloud_project_model_id
   end
 end
