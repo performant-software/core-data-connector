b62123bc066b4cd9e630e89c6c3b810066122779
Rebecca Black <rebecca.ann.black@gmail.com>
2025-03-07 13:45:24 -0500

    add separate year facet

--- app/services/core_data_connector/search/event.rb
+++ app/services/core_data_connector/search/event.rb
@@ -17,12 +17,20 @@ module CoreDataConnector
         search_attribute :name, facet: true
         search_attribute :description
 
-        search_attribute(:start_date, facet: true) do
+        search_attribute(:start_date) do
           resolve_date start_date
         end
 
-        search_attribute(:end_date, facet: true) do
+        search_attribute(:end_date) do
           resolve_date end_date
+        end
+
+        search_attribute(:start_year, facet: true) do
+          resolve_year start_date
+        end
+
+        search_attribute(:end_year, facet: true) do
+          resolve_year end_date
         end
 
         private
@@ -30,7 +38,13 @@ module CoreDataConnector
         def resolve_date(date)
           return [] unless date.present?
 
-          [date.start_date&.to_fs(:year)&.to_i, date.end_date&.to_fs(:year)&.to_i]
+          [date.start_date&.to_time&.to_i, date.end_date&.to_time&.to_i]
+        end
+
+        def resolve_year(date)
+          return [] unless date.present?
+
+          [date.start_date&.strftime('%Y')&.to_i, date.end_date&.strftime('%Y')&.to_i]
         end
 
       end
