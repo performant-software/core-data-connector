48691615829bef51a35846236001be317d04b9c5
Camden Mecklem <c@camdenmecklem.com>
2025-04-09 12:01:09 -0400

    make order optional

--- db/migrate/20250407150531_add_order_to_relationships.rb
+++ db/migrate/20250407150531_add_order_to_relationships.rb
@@ -1,5 +1,5 @@
 class AddOrderToRelationships < ActiveRecord::Migration[7.0]
   def change
-    add_column :core_data_connector_relationships, :order, :integer, default: 0
+    add_column :core_data_connector_relationships, :order, :integer
   end
 end
