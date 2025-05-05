f3f30fba077ec2732133befa36a338e9869b2816
Camden <c@camdenmecklem.com>
2025-05-05 11:20:29 -0400

    Update relationship manifests when a `MediaContent` record's UDFs are updated (#146)
    * update manifests when media contents UDFs update
    
    * feedback and cleanup
    
    * renaming

--- app/models/core_data_connector/media_content.rb
+++ app/models/core_data_connector/media_content.rb
@@ -10,6 +10,31 @@ module CoreDataConnector
     include TripleEyeEffable::Resourceable
     include UserDefinedFields::Fieldable
 
+    after_save :update_manifests
+
+    def update_manifests
+      iiif_service = Iiif::Manifest.new
+
+      self.relationships.each { |r| update_relationship_manifests(r, iiif_service, true) }
+      self.related_relationships.each { |r| update_relationship_manifests(r, iiif_service, false) }
+    end
+
+    def update_relationship_manifests(relationship, service, is_primary)
+      if is_primary
+        related_model = relationship.related_record_type.constantize
+        related_record_id = relationship.related_record_id
+      else
+        related_model = relationship.primary_record_type.constantize
+        related_record_id = relationship.primary_record_id
+      end
+
+      service.reset_manifests_by_type(related_model, {
+        id: related_record_id,
+        project_model_relationship_id: relationship.project_model_relationship_id,
+        limit: ENV['IIIF_MANIFEST_ITEM_LIMIT']
+      })
+    end
+
     def metadata
       if !self.user_defined || self.user_defined.keys.count == 0
         return '[]'
