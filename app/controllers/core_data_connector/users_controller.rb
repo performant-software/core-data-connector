b7a409e378ee9f39c5e80740777e89e5f4c25da5
dleadbetter <derek@performantsoftware.com>
2025-06-06 15:18:54 -0400

    CDC #443 - Updating users_controller to preload user_projects, project on show action

--- app/controllers/core_data_connector/users_controller.rb
+++ app/controllers/core_data_connector/users_controller.rb
@@ -1,5 +1,9 @@
 module CoreDataConnector
   class UsersController < ApplicationController
+    # Search attributes
     search_attributes :name, :email
+
+    # Preloads
+    preloads user_projects: :project, only: :show
   end
 end
