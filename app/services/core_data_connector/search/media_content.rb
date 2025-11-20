bd05fc2dce60addb42be560517f747f352228695
Camden <c@camdenmecklem.com>
2025-11-20 12:06:27 -0500

    add thumbnail to media content record index hash (#174)

--- app/services/core_data_connector/search/media_content.rb
+++ app/services/core_data_connector/search/media_content.rb
@@ -9,6 +9,10 @@ module CoreDataConnector
 
         # Search attributes
         search_attribute :name
+
+        search_attribute(:thumbnail) do
+          content_thumbnail_url
+        end
       end
 
     end
