8b4d88d67c600f52ddb546b592b59993debdddc2
Ben Silverman <ben@performantsoftware.com>
2026-02-25 13:53:45 -0500

    Ensure temporary password meets reqs (#555)

--- app/services/core_data_connector/users/passwords.rb
+++ app/services/core_data_connector/users/passwords.rb
@@ -2,16 +2,17 @@ module CoreDataConnector
   module Users
     class Passwords
 
+      # default lengths of generated passwords
       SSO_PASSWORD_LENGTH = 50
-      USER_PASSWORD_LENGTH = 6
+      USER_PASSWORD_LENGTH = 16
 
-      # Password format
+      # Password format; must match frontend validation regex
       PASSWORD_FORMAT = /\A
-        (?=.{8,})          # Must contain 8 or more characters
-        (?=.*\d)           # Must contain a digit
-        (?=.*[a-z])        # Must contain a lower case character
-        (?=.*[A-Z])        # Must contain an upper case character
-        (?=.*[[:^alnum:]]) # Must contain a symbol
+        (?=.{8,})            # Must contain 8 or more characters
+        (?=.*\d)             # Must contain a digit
+        (?=.*[a-z])          # Must contain a lower case character
+        (?=.*[A-Z])          # Must contain an upper case character
+        (?=.*[^a-zA-Z0-9\s]) # Must contain a symbol
       /x
 
       def self.generate_sso_password
@@ -25,9 +26,21 @@ module CoreDataConnector
       private
 
       def self.generate_password(length)
-        SecureRandom.base64(length)
-      end
+        # generate a password that is guaranteed to match regex
+        lower = ('a'..'z').to_a
+        upper = ('A'..'Z').to_a
+        numbers = ('0'..'9').to_a
+        symbols = %w[@ $ ! % * ? &]
+        all_allowed = lower + upper + numbers + symbols
 
+        # random mix of allowed characters (ensure at least one of each required type)
+        required = [lower.sample, upper.sample, numbers.sample, symbols.sample]
+        rest = Array.new(length - 4) { all_allowed.sample }
+        password_chars = required + rest
+  
+        # shuffle so that first 4 aren't always "lower upper number symbol"
+        password_chars.shuffle.join
+      end
     end
   end
 end
