090823d9bf67026245f077de50a30e6a86f0fe01
Ben Silverman <ben@performantsoftware.com>
2026-05-12 17:16:10 -0400

    Allow Reconciliation filtering by project_model_id ref performant-software/core-data-cloud#613

--- app/services/core_data_connector/reconcile/manager.rb
+++ app/services/core_data_connector/reconcile/manager.rb
@@ -28,8 +28,28 @@ module CoreDataConnector
 
           # Append "filter_by" parameter, if present
           if query[:type].present?
-            types = Array.wrap(query[:type]).join(',')
-            search[:filter_by] = "type:[#{types}]"
+            types = Array.wrap(query[:type])
+
+            # filter by project model ID and/or model_class
+            project_model_ids = []
+            class_names = []
+            types.each do |t|
+              if t.start_with?('model:')
+                project_model_ids << t.split(':', 2).last
+              elsif t.start_with?('type:')
+                class_names << t.split(':', 2).last
+              else
+                class_names << t
+              end
+            end
+
+            # collect into typesense filters
+            filters = []
+            filters << "type:[#{class_names.join(',')}]" if class_names.any?
+            filters << "project_model_id:[#{project_model_ids.join(',')}]" if project_model_ids.any?
+
+            # join by typesense OR
+            search[:filter_by] = filters.join(' || ') if filters.any?
           end
 
           # Append "limit" parameter, if present
@@ -53,14 +73,26 @@ module CoreDataConnector
 
       def transform_result(result)
         klass = result['document']['type'].constantize
+        project_model_id = result['document']['project_model_id']
 
         attributes = {
           score: result['text_match'],
           match: false,
-          type: [{
-            id: klass.to_s,
-            name: klass.name.demodulize
-          }]
+          type: [
+            {
+              id: "type:#{klass.to_s}",
+              name: klass.name.demodulize
+            },
+            {
+              id: "model:#{project_model_id}",
+              name: "#{klass.name.demodulize} model #{project_model_id}"
+            },
+            # retain bare model_class string for backwards compatibility
+            {
+              id: klass.to_s,
+              name: klass.name.demodulize
+            }
+          ]
         }
 
         all_attributes = result['document'].merge(attributes).symbolize_keys
