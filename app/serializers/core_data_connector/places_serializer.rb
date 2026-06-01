a45d766f097147026529c338d832d574b64281eb
Camden Mecklem <c@camdenmecklem.com>
2026-06-01 16:19:25 -0400

    cleanup

--- app/serializers/core_data_connector/places_serializer.rb
+++ app/serializers/core_data_connector/places_serializer.rb
@@ -3,7 +3,6 @@ module CoreDataConnector
     include OwnableSerializer
     include UserDefinedFields::FieldableSerializer
     include RelatedColumnsSerializable
-    include RelatedColumnsSerializable
 
     index_attributes :id, :name
     show_attributes :id, :name, place_names: [:id, :name, :primary], place_geometry: PlaceGeometriesSerializer,
