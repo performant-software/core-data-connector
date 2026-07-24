ab94fa54ea08e2a8acd8d37d162bf298f308fbdb
Camden Mecklem <c@camdenmecklem.com>
2026-07-24 11:04:57 -0400

    remove dead code

--- app/models/core_data_connector/version.rb
+++ app/models/core_data_connector/version.rb
@@ -9,12 +9,5 @@ module CoreDataConnector
     def self.for_record(record)
       where('roots @> ?', [{ type: record.class.name, id: record.id }].to_json)
     end
-
-    # Group by request_uuid so we can combine changes to multiple tables that were semantically
-    # part of the same record action. (e.g. place name + geometry)
-    def self.changesets
-      order(created_at: :desc, id: :desc)
-        .group_by { |version| version.request_uuid }
-    end
   end
 end
